ac4e89f435da3412bed6204c8ac8ea511698acff
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-07-07 15:37:50 -0700

    Added routes for login and signup, and a route whit dynamic content based on user

--- upsolver-ui/src/components/App/App.jsx
+++ upsolver-ui/src/components/App/App.jsx
@@ -1,10 +1,69 @@
 import React from "react";
 import "./App.css";
+import { useState, useEffect } from "react";
+import { UserContext } from "../../UserContext";
+import LoginForm from "../LoginForm/LoginForm";
+import SignupForm from "../SignupForm/SignupForm";
+import { Container, Typography } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
+  const [user, setUser] = useState(() => {
+    const savedUser = localStorage.getItem("user");
+    return savedUser ? JSON.parse(savedUser) : null;
+  });
+
+  const updateUser = (newUser) => {
+    setUser(newUser);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(user));
+  }, [user]);
+
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <UserContext.Provider value={{ user, updateUser }}>
+        <BrowserRouter>
+          <Routes basename="/">
+            <Route
+              path="/"
+              element={
+                <Container
+                  maxWidth="gx"
+                  sx={{ display: "flex", justifyContent: "center" }}
+                >
+                  <Typography variant="h2">
+                    Welcome to Upsolver {user ? user.username : "Guest"}
+                  </Typography>
+                </Container>
+              }
+            />
+            <Route
+              path="login"
+              element={
+                <Container
+                  maxWidth="gx"
+                  sx={{ display: "flex", justifyContent: "center" }}
+                >
+                  <LoginForm />
+                </Container>
+              }
+            />
+            <Route
+              path="signup"
+              element={
+                <Container
+                  maxWidth="gx"
+                  sx={{ display: "flex", justifyContent: "center" }}
+                >
+                  <SignupForm />
+                </Container>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </UserContext.Provider>
     </div>
   );
 }

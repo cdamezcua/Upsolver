3b79962f31a87c4eb2d3bfe7cb48c86342768453
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-07-07 15:32:55 -0700

    Updated CORS policy allowing requests from lh:3000

--- upsolver-api/server.js
+++ upsolver-api/server.js
@@ -1,10 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import router from "./routes/index.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
 const port = process.env.PORT || 3001;
 
 app.use(express.json());

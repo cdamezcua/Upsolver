f88c2bae82a52e5946efddc210466a5d93d854a2
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-07-09 22:23:28 -0700

    Added role and team model to index exports

--- upsolver-api/models/index.js
+++ upsolver-api/models/index.js
@@ -1,5 +1,6 @@
 import { User } from "./user.js";
+import { Role } from "./role.js";
+import { Team } from "./team.js";
 import { InvalidatedJWT } from "./invalidatedJWT.js";
 
-export { User, InvalidatedJWT };
-
+export { User, Role, Team, InvalidatedJWT };

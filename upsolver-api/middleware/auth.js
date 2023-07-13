3cba2fd8e774cbb78b03331ce2f9219be85aaeab
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-07-13 05:45:17 -0700

    Refactored auth m. to use AppError, and tryCatck

--- upsolver-api/middleware/auth.js
+++ upsolver-api/middleware/auth.js
@@ -2,26 +2,25 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import { InvalidatedJWT } from "../models/index.js";
 import { HEADER_TOKEN_KEY } from "../constants/config.js";
+import tryCatch from "../utils/tryCatch.js";
+import AppError from "../utils/AppError.js";
 
 dotenv.config();
 
-const verifyToken = async (req, res, next) => {
+const verifyToken = tryCatch(async (req, res, next) => {
   const token =
     req.body.token || req.query.token || req.headers[HEADER_TOKEN_KEY];
   if (!token) {
-    return res.status(401).send("[!] A token is required for authentication");
+    throw new AppError(401, "A token is required for authentication");
   }
-  try {
-    const decoded = jwt.verify(token, process.env.TOKEN_KEY);
-    const invalidated = await InvalidatedJWT.findOne({ where: { token } });
-    if (invalidated) {
-      return res.status(401).send("[!] Invalidated token");
-    }
-    req.user = decoded;
-  } catch (err) {
-    return res.status(401).send("[!] Invalid Token");
+  req.token = token;
+  const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+  const denylisted = await InvalidatedJWT.findOne({ where: { token } });
+  if (denylisted) {
+    throw new AppError(401, "Denylisted token");
   }
+  req.user = decoded;
   return next();
-};
+});
 
 export default verifyToken;

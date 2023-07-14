96b5fdeca792d533b3e1c092c1d59d12ef37f146
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-07-14 11:39:20 -0700

    Created endpoint to accept an incomming invitation

--- upsolver-api/routes/invitations.js
+++ upsolver-api/routes/invitations.js
@@ -3,6 +3,7 @@ import verifyToken from "../middleware/auth.js";
 import { sequelize } from "../database.js";
 import tryCatch from "../utils/tryCatch.js";
 import errorHandler from "../middleware/errorHandler.js";
+import AppError from "../utils/AppError.js";
 
 const router = express.Router();
 
@@ -32,6 +33,69 @@ router.get(
       type: sequelize.QueryTypes.SELECT,
     });
     res.status(200).json({ invitations });
+  })
+);
+
+router.post(
+  "/:invitationId/accept",
+  verifyToken,
+  tryCatch(async (req, res) => {
+    const getInvetee = `
+        SELECT
+            inviteeId 
+        FROM
+            invitations
+        WHERE
+            id = ${req.params.invitationId};
+    `;
+    const [invitee] = await sequelize.query(getInvetee, {
+      type: sequelize.QueryTypes.SELECT,
+    });
+    const inviteeId = invitee.inviteeId;
+    if (inviteeId !== req.user.user_id) {
+      throw new AppError(
+        403,
+        "You are not allowed to accept other's invitations"
+      );
+    }
+    const getInvitation = `
+        SELECT
+            id
+            ,teamId
+            ,role
+        FROM
+            invitations
+        WHERE
+            id = ${req.params.invitationId};
+    `;
+    const [invitation] = await sequelize.query(getInvitation, {
+      type: sequelize.QueryTypes.SELECT,
+    });
+    const teamId = invitation.teamId;
+    const role = invitation.role;
+    const addMember = `
+        INSERT INTO
+            roles (userId, teamId, name)
+        VALUES
+            (${req.user.user_id}, ${teamId}, '${role}')
+        ON DUPLICATE KEY UPDATE
+            name = '${role}';      
+    `;
+
+    await sequelize.query(addMember, {
+      type: sequelize.QueryTypes.INSERT,
+    });
+
+    const deleteInvitation = `
+        DELETE FROM
+            invitations
+        WHERE
+            id = ${req.params.invitationId};
+    `;
+    await sequelize.query(deleteInvitation, {
+      type: sequelize.QueryTypes.DELETE,
+    });
+    res.status(200).json({ message: "Invitation accepted" });
   })
 );
 

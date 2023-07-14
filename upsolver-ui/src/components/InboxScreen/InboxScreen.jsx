2bc11a001a7da1c06420843595112d3a1d73b0f1
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-07-14 12:11:07 -0700

    Created InboxScreen component

---
+++ upsolver-ui/src/components/InboxScreen/InboxScreen.jsx
@@ -0,0 +1,151 @@
+import React from "react";
+import "./InboxScreen.css";
+import Navbar from "../Navbar/Navbar";
+import { Box, Divider, IconButton, Tab, Typography } from "@mui/material";
+import { Button, Stack, Avatar } from "@mui/material";
+import { Link } from "react-router-dom";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
+import { UserContext } from "../../UserContext.js";
+import { useEffect, useContext } from "react";
+import RefreshIcon from "@mui/icons-material/Refresh";
+import Subtitle from "../Subtitle/Subtitle";
+
+export default function InvoxScreen() {
+  const { user } = useContext(UserContext);
+
+  const [invitations, setInvitations] = React.useState([]);
+
+  async function fetchInvitations() {
+    try {
+      const response = await fetch("http://localhost:3001/invitations", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "x-access-token": user?.token,
+        },
+      });
+      const data = await response.json();
+      setInvitations(data.invitations ?? []);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchInvitations();
+  }, [user]);
+
+  async function acceptInvitation(invitationId) {
+    try {
+      const response = await fetch(
+        "http://localhost:3001/invitations/" + invitationId + "/accept",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "x-access-token": user?.token,
+          },
+        }
+      );
+      const data = await response.json();
+      fetchInvitations();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function rejectInvitation(invitationId) {
+    try {
+      const response = await fetch(
+        "http://localhost:3001/invitations/" + invitationId + "/reject",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "x-access-token": user?.token,
+          },
+        }
+      );
+      const data = await response.json();
+      fetchInvitations();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <>
+      <Navbar />
+      <Subtitle>
+        Inbox
+      </Subtitle>
+      <Box sx={{ m: "20px" }}>
+        <TableContainer component={Paper}>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              m: "20px",
+            }}
+          >
+            <Typography variant="h6">pending Invitations</Typography>
+            <IconButton onClick={fetchInvitations} size="large">
+              <RefreshIcon />
+            </IconButton>
+          </Box>
+          <Divider />
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Inviter</TableCell>
+                <TableCell>Team</TableCell>
+                <TableCell>Role</TableCell>
+                <TableCell>Invited On</TableCell>
+                <TableCell>Actions</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {invitations.map((invitation) => (
+                <TableRow key={invitation.id}>
+                  <TableCell>{invitation.inviter}</TableCell>
+                  <TableCell>{invitation.team}</TableCell>
+                  <TableCell>{invitation.role}</TableCell>
+                  <TableCell>
+                    {new Date(invitation.createdAt).toLocaleDateString()}
+                  </TableCell>
+                  <TableCell>
+                    <Stack direction="row" spacing={2}>
+                      <Button
+                        variant="contained"
+                        color="success"
+                        onClick={() => acceptInvitation(invitation.id)}
+                      >
+                        Accept
+                      </Button>
+                      <Button
+                        variant="contained"
+                        color="error"
+                        onClick={() => rejectInvitation(invitation.id)}
+                      >
+                        Reject
+                      </Button>
+                    </Stack>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </>
+  );
+}

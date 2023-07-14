4853dec87bcf306fdf10a9a3926e1df8805a3074
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-07-14 14:39:31 -0700

    Created placeholder button to create a team

--- upsolver-ui/src/components/TeamsScreen/TeamsScreen.jsx
+++ upsolver-ui/src/components/TeamsScreen/TeamsScreen.jsx
@@ -3,15 +3,22 @@ import "./TeamsScreen.css";
 import Navbar from "../Navbar/Navbar";
 import TeamGrid from "../TeamGrid/TeamGrid";
 import Subtitle from "../Subtitle/Subtitle";
+import { Fab } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 
 export default function TeamsScreen() {
   return (
     <div className="teams-screen-container">
       <Navbar />
-      <Subtitle>
-        My Teams
-      </Subtitle>
+      <Subtitle>My Teams</Subtitle>
       <TeamGrid />
+      <Fab
+        className="create-team-button"
+        color="primary"
+        onClick={() => console.log("create team")}
+      >
+        <AddIcon />
+      </Fab>
     </div>
   );
 }

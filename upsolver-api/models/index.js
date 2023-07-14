a00a2bc1193e635d27f7279c6f2117cfccdd57ab
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-07-14 11:27:54 -0700

    Indexed new Invitation model

--- upsolver-api/models/index.js
+++ upsolver-api/models/index.js
@@ -3,5 +3,6 @@ import { User } from "./user.js";
 import { Role } from "./role.js";
 import { Team } from "./team.js";
 import { Group } from "./group.js";
+import { Invitation } from "./invitation.js";
 
-export { InvalidatedJWT, User, Role, Team, Group };
+export { InvalidatedJWT, User, Role, Team, Group, Invitation };

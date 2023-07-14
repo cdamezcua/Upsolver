be8176e4320fa1d1a9f2919fe0993d2210f8ba25
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-07-14 11:40:25 -0700

    Indexed invitation endpoints

--- upsolver-api/routes/index.js
+++ upsolver-api/routes/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import usersRoutes from "./users.js";
 import teamsRoutes from "./teams.js";
 import groupsRoutes from "./groups.js";
+import invitationsRoutes from "./invitations.js";
 
 const router = express.Router();
 
 router.use("/users", usersRoutes);
 router.use("/teams", teamsRoutes);
 router.use("/teams", groupsRoutes);
+router.use("/invitations", invitationsRoutes);
 
 export default router;

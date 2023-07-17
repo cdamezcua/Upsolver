c0dd077f6dab54206d165d3df09f49702a8d1b33
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-07-17 06:23:04 -0700

    Now username's color match user's cf rank

--- upsolver-ui/src/components/UserNavCard/UserNavCard.jsx
+++ upsolver-ui/src/components/UserNavCard/UserNavCard.jsx
@@ -2,16 +2,22 @@ import React, { useContext } from "react";
 import "./UserNavCard.css";
 import { Box, Avatar, Typography } from "@mui/material";
 import { UserContext } from "../../UserContext.js";
+import { RANKING_COLORS } from "../../constants/config.js";
 
 export default function UserNavCard() {
   const { user } = useContext(UserContext);
-
   return (
     <Box className="user-nav-card-container">
       <Box className="avatar-container">
         <Avatar variant="square">{user.username[0]}</Avatar>
       </Box>
-      <Typography className="username" variant="h6" color="text.primary">{user.username}</Typography>
+      <Typography
+        className="username"
+        variant="h6"
+        color={RANKING_COLORS[user.rank]}
+      >
+        {user.username}
+      </Typography>
     </Box>
   );
 }

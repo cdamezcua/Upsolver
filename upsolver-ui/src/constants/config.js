b89e3a1d3e02e94f0c29beb876fa9b08d4834482
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-07-17 06:14:34 -0700

    Created constant that maps cf ranks to colors

---
+++ upsolver-ui/src/constants/config.js
@@ -0,0 +1,13 @@
+export const RANKING_COLORS = {
+  unrated: "#000000",
+  newbie: "#808080",
+  pupil: "#008000",
+  specialist: "#03a89e",
+  expert: "#0000ff",
+  "candidate master": "#800080",
+  master: "#ffa500",
+  "international master": "#ffa500",
+  grandmaster: "#ff0000",
+  "international grandmaster": "#ff0000",
+  "legendary grandmaster": "#ff0000",
+};

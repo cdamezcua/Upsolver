4a23c339fc4312c4bed84bfe194ec41ba3c65e6c
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-07-19 10:48:40 -0700

    Added constant with the url of flask server

--- upsolver-api/constants/urls.js
+++ upsolver-api/constants/urls.js
@@ -3,3 +3,5 @@ import { FRONT_END_PORT, PORT } from "./config.js";
 export const FRONT_END_BASE_URL = "http://localhost:" + FRONT_END_PORT;
 
 export const BASE_URL = "http://localhost:" + PORT;
+
+export const FLASK_API_URL = "https://upsolver-flask.vercel.app/";

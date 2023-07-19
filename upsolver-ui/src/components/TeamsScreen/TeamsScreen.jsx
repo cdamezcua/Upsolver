9941d7a8db5be812ee564c5425d7921e834df0d9
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-07-19 10:57:43 -0700

    Added function to fetch teams and passed teams to teamGrid as property

--- upsolver-ui/src/components/TeamsScreen/TeamsScreen.jsx
+++ upsolver-ui/src/components/TeamsScreen/TeamsScreen.jsx
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
 import "./TeamsScreen.css";
 import Navbar from "../Navbar/Navbar";
 import TeamGrid from "../TeamGrid/TeamGrid";
 import Subtitle from "../Subtitle/Subtitle";
 import { Fab } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
+import { UserContext } from "../../UserContext.js";
 
 export default function TeamsScreen() {
+  const { user } = useContext(UserContext);
+
+  const [teams, setTeams] = React.useState([]);
+
+  async function fetchTeams() {
+    try {
+      const response = await fetch("http://localhost:3001/teams", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "x-access-token": user?.token,
+        },
+      });
+      const data = await response.json();
+      setTeams(data.teams ?? []);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchTeams();
+  }, [user]);
+
   return (
     <div className="teams-screen-container">
       <Navbar />
       <Subtitle>My Teams</Subtitle>
-      <TeamGrid />
+      <TeamGrid teams={teams} />
       <Fab
         className="create-team-button"
         color="primary"

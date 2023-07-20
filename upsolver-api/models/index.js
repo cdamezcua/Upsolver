993ee786e09c2f82562b47e9c64208240d15d14f
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-07-20 12:44:52 -0700

    Indexed contest and problem models

--- upsolver-api/models/index.js
+++ upsolver-api/models/index.js
@@ -4,5 +4,16 @@ import { Role } from "./role.js";
 import { Team } from "./team.js";
 import { Group } from "./group.js";
 import { Invitation } from "./invitation.js";
+import { Contest } from "./contest.js";
+import { Problem } from "./problem.js";
 
-export { InvalidatedJWT, User, Role, Team, Group, Invitation };
+export {
+  InvalidatedJWT,
+  User,
+  Role,
+  Team,
+  Group,
+  Invitation,
+  Contest,
+  Problem,
+};

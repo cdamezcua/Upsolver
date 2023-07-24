ee789f3791fe18932d689d2534f0865456f92179
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-07-24 12:02:08 -0700

    created endpoint to get all the sumbissions of all contestants in a group for all the problems in the group

---
+++ upsolver-api/routes/submissions.js
@@ -0,0 +1,75 @@
+import express from "express";
+import verifyToken from "../middleware/auth.js";
+import { sequelize } from "../database.js";
+import { Submission } from "../models/submission.js";
+import errorHandler from "../middleware/errorHandler.js";
+import tryCatch from "../utils/tryCatch.js";
+import verifyTeamMembership from "../middleware/verifyTeamMembership.js";
+
+const router = express.Router();
+
+router.get(
+  "/:teamId/groups/:groupId/submissions",
+  verifyToken,
+  verifyTeamMembership,
+  tryCatch(async (req, res) => {
+    const { groupId } = req.params;
+    const { membership } = req.query;
+    const problemsOfGroup = `
+        SELECT
+            P.id AS problemId
+        FROM
+            problems P
+            INNER JOIN contests C
+                ON P.contestId = C.id
+            INNER JOIN groups G
+                ON C.groupId = G.id
+        WHERE
+            G.id = ${groupId}
+    `;
+    const membersOfGroup = `
+        SELECT
+            U.id AS userId
+        FROM
+            users U
+            INNER JOIN roles R
+                ON R.userId = U.id
+            INNER JOIN teams T
+                ON T.id = R.teamId
+            INNER JOIN groups G
+                ON G.teamId = T.id
+        WHERE
+            G.id = ${groupId}
+            AND (R.name = '${membership}' 
+                OR '${membership}' = 'undefined')
+    `;
+    const submissionsOfMembersOfGroupToProblemsOfGroup = `
+        SELECT
+            U.id AS userId
+            ,P.id AS problemId
+            ,S.veredict AS veredict
+        FROM
+            users U
+            CROSS JOIN problems P
+            LEFT JOIN submissions S
+                ON S.userId = U.id
+                AND S.problemId = P.id
+        WHERE
+            U.id IN (${membersOfGroup})
+            AND P.id IN (${problemsOfGroup});
+    `;
+    const submissions = await sequelize.query(
+      submissionsOfMembersOfGroupToProblemsOfGroup,
+      {
+        type: sequelize.QueryTypes.SELECT,
+        model: Submission,
+        mapToModel: true,
+      }
+    );
+    res.status(200).json({ submissions });
+  })
+);
+
+router.use(errorHandler);
+
+export default router;

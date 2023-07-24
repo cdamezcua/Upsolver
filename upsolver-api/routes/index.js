a1a09844ec64adbaa32b47f923cc31e72a316e28
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-07-24 12:04:37 -0700

    Indexed new problem and submission routes

--- upsolver-api/routes/index.js
+++ upsolver-api/routes/index.js
@@ -4,9 +4,13 @@ import teamsRoutes from "./teams.js";
 import groupsRoutes from "./groups.js";
 import invitationsRoutes from "./invitations.js";
 import contestsRoutes from "./contests.js";
+import problemsRoutes from "./problems.js";
+import submissionssRoutes from "./submissions.js";
 
 const router = express.Router();
 
+router.use("/teams", submissionssRoutes);
+router.use("/teams", problemsRoutes);
 router.use("/teams", contestsRoutes);
 router.use("/teams", groupsRoutes);
 router.use("/teams", teamsRoutes);

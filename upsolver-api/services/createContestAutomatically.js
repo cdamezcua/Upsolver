290a73aaf4d8bbea7dc27da2a3d5273e1167179d
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-07-24 11:57:36 -0700

    Modified createContest service to also create the problems of contest returning their ids in response

--- upsolver-api/services/createContestAutomatically.js
+++ upsolver-api/services/createContestAutomatically.js
@@ -3,10 +3,7 @@ import { FLASK_API_URL } from "../constants/urls.js";
 import AppError from "../utils/AppError.js";
 import { Contest } from "../models/index.js";
 
-const createContestAutomatically = async (
-  groupId,
-  contest_constructor
-) => {
+const createContestAutomatically = async (groupId, contest_constructor) => {
   try {
     const response = await fetch(FLASK_API_URL + "contests", {
       method: "POST",
@@ -24,17 +21,36 @@ const createContestAutomatically = async (
                 (groupId, url, name, start_time, division, number)
             VALUES
                 (${groupId}, '${contest.url}', '${contest.name}', '${contest.start_time}', '${contest.division}', '${contest.number}');`;
-    const [createdContest, created] = await sequelize.query(createContest, {
+    const [[createdContest], created] = await sequelize.query(createContest, {
       type: sequelize.QueryTypes.INSERT,
       model: Contest,
       mapToModel: true,
     });
 
+    const problems = contest.problems;
+
+    const createProblemsPromises = await problems.map(async (problem) => {
+      const createProblem = `
+                INSERT INTO problems
+                    (contestId, url, number, name, solved_count)
+                VALUES
+                    (${createdContest.id}, '${problem.url}', '${problem.number}', '${problem.name}', ${problem.solved_count});`;
+      const [[createdProblem], created] = await sequelize.query(createProblem, {
+        type: sequelize.QueryTypes.INSERT,
+        model: Contest,
+        mapToModel: true,
+      });
+
+      return createdProblem.id;
+    });
+
+    const createdProblemIds = await Promise.all(createProblemsPromises);
+
     if (!created) {
       throw new AppError(500, "Failed to create contest");
     }
 
-    return createdContest;
+    return { id: createdContest.id, problems: createdProblemIds };
   } catch (err) {
     return err;
   }

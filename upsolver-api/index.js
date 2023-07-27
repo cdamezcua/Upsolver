c2db1b7c89a05ef16ab5dd097f2e4d43af7c9a1c
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-07-26 23:04:05 -0700

    Initialized a socket server with events for connection and disconection, joining and leaving a room and sending messages

--- upsolver-api/index.js
+++ upsolver-api/index.js
@@ -2,7 +2,12 @@ import express from "express";
 import router from "./routes/index.js";
 import cors from "cors";
 import { FRONT_END_BASE_URL } from "./constants/urls.js";
-import { PORT } from "./constants/config.js";
+import { PORT, SOCKET_PORT } from "./constants/config.js";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
@@ -13,9 +18,37 @@ app.use(
   })
 );
 
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: FRONT_END_BASE_URL,
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("join", async (room) => {
+    socket.join(room.id);
+  });
+
+  socket.on("message", (message) => {
+    io.to(message.roomId).emit("message", message);
+  });
+
+  socket.on("leave", (room) => {
+    socket.leave(room.id);
+  });
+
+  socket.on("disconnect", () => {});
+});
+
 app.use(express.json());
 app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
+});
+
+httpServer.listen(3002, () => {
+  console.log(`Listening on port ${SOCKET_PORT}`);
 });

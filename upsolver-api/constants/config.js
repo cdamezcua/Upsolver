6904a3be588361b9d3c6cb95d8067c15e82fe466
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-07-26 23:09:03 -0700

    Added a constant for socket port

--- upsolver-api/constants/config.js
+++ upsolver-api/constants/config.js
@@ -4,6 +4,7 @@ dotenv.config();
 
 export const FRONT_END_PORT = process.env.FRONT_END_PORT || 3000;
 export const PORT = process.env.PORT || 3001;
+export const SOCKET_PORT = process.env.SOCKET_PORT || 3002;
 export const ROUNDS_OF_HASHING = 10;
 export const TOKEN_LIFETIME = "24h";
 export const HEADER_TOKEN_KEY = "x-access-token";

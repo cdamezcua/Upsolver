3e2f68b98113d4cdd82a776c6906aa19a1ab1120
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-08-01 23:19:50 -0700

    Refactored Group constructor to parse html code with an automaton instead of with BeautifulSoup

--- upsolver-flask/api/index.py
+++ upsolver-flask/api/index.py
@@ -22,26 +22,94 @@ class Group:
         self.url = group_constructor["url"]
         response = http.get(self.url)
         soup = BeautifulSoup(response.text, "html.parser")
-        self.name = soup.select_one("#sidebar th a").string.strip()
+        document = soup.prettify()
+        state = 0
+        rowIndex = 0
+        colIndex = 0
+        divDepth = 0
+        href = ""
+        contest_url = ""
+        contest_start_time = ""
         self.contest_constructors = []
-        contest_table = soup.find("div", class_="contests-table")
-        if contest_table:
-            table_rows = contest_table.find_all("tr")
-            for table_row in table_rows[1:]:
-                table_cells = table_row.find_all("td")
-                contest_url = table_cells[0].find(
-                    "a", string=lambda string: string and "Enter »" in string
-                )["href"]
-                if not contest_url.startswith(URL_PREFIX):
-                    contest_url = URL_PREFIX + contest_url
-                contest_start_time = table_cells[1].find("span", class_="format-time")
-                if contest_start_time is not None:
-                    contest_start_time = contest_start_time.string.strip()
-                else:
-                    contest_start_time = "-"
-                self.contest_constructors.append(
-                    {"url": contest_url, "start_time": contest_start_time}
-                )
+        for line in document.split("\n"):
+            line = line.strip()
+            if state == 0:  # in document looking for contests-table or sidebar
+                if line.startswith("<div") and "contests-table" in line:
+                    rowIndex = 0
+                    state = 1
+                    divDepth = 1
+                elif line.startswith("<div") and 'id="sidebar"' in line:
+                    state = 6
+                    divDepth = 1
+            elif state == 1:  # in contests-table looking for every tr
+                if line.startswith("<div"):
+                    divDepth += 1
+                elif line.startswith("</div"):
+                    divDepth -= 1
+                    if divDepth == 0:
+                        state = 0
+                elif line.startswith("<tr"):
+                    colIndex = 0
+                    state = 2
+            elif state == 2:  # in contest-table>tr looking for every td
+                if line.startswith("</tr>"):
+                    rowIndex += 1
+                    if contest_url != "":
+                        self.contest_constructors.append(
+                            {
+                                "url": contest_url,
+                                "start_time": contest_start_time or "-",
+                            }
+                        )
+                    contest_url = ""
+                    contest_start_time = ""
+                    state = 1
+                elif rowIndex == 0:  # ignore the first row
+                    continue
+                elif line.startswith("<td"):
+                    state = 3
+            elif state == 3:  # in contest-table>tr>td looking for info
+                if line.startswith("</td>"):
+                    colIndex += 1
+                    state = 2
+                elif 0 <= colIndex and colIndex <= 1:
+                    if line.startswith("<a"):
+                        href = re.search(r'href="(.+?)"', line).group(1)
+                        state = 4
+            elif state == 4:  # in contest-table>tr>td>a looking for c. url and span
+                if line.startswith("</a>"):
+                    state = 3
+                elif not line.startswith("<") and line.strip() == "Enter »":
+                    contest_url = href
+                    if not contest_url.startswith(URL_PREFIX):
+                        contest_url = URL_PREFIX + contest_url
+                    state = 3
+                elif line.startswith("<span"):
+                    state = 5
+            elif state == 5:  # in contest-table>tr>td>a>span looking for start time
+                if line.startswith("</span>"):
+                    state = 4
+                elif not line.startswith("<"):
+                    contest_start_time = line.strip()
+            elif state == 6:  # in sidebar looking for th
+                if line.startswith("<div"):
+                    divDepth += 1
+                elif line.startswith("</div"):
+                    divDepth -= 1
+                    if divDepth == 0:
+                        state = 0
+                elif line.startswith("<th"):
+                    state = 7
+            elif state == 7:  # in sidebar>th looking for a
+                if line.startswith("</th>"):
+                    state = 6
+                elif line.startswith("<a"):
+                    state = 8
+            elif state == 8:  # in sidebar>th>a looking for group name
+                if line.startswith("</a>"):
+                    state = 7
+                elif not line.startswith("<"):
+                    self.name = line.strip()
         self.json = {
             "url": self.url,
             "name": self.name,

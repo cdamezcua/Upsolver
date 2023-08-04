3518324b8f2f3358c583b3d31b1ce1c069dc38c0
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-08-03 23:32:51 -0700

    Now profile screen only shows userCard

--- upsolver-ui/src/components/ProfileScreen/ProfileScreen.jsx
+++ upsolver-ui/src/components/ProfileScreen/ProfileScreen.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import "./ProfileScreen.css";
 import Navbar from "../Navbar/Navbar";
-import { Box, Typography } from "@mui/material";
-import { Paper, Grid } from "@mui/material";
+import { Box } from "@mui/material";
 import { UserContext } from "../../UserContext.js";
 import { useContext } from "react";
 import Subtitle from "../Subtitle/Subtitle";
@@ -15,33 +14,9 @@ export default function ProfileScreen() {
     <>
       <Navbar />
       <Subtitle>Profile</Subtitle>
-      <Box sx={{ m: "50px" }}>
-        <Grid container>
-          <Grid
-            Paper
-            xs={4}
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <UserCard />
-          </Grid>
-          <Grid Paper xs={8}>
-            <Paper>
-              <Typography variant="h6">Country: {user.country}</Typography>
-              <Typography variant="h6">City: {user.city}</Typography>
-              <Typography variant="h6">Email: {user.email}</Typography>
-            </Paper>
-          </Grid>
-          <Grid Paper xs={4}>
-            <Paper></Paper>
-          </Grid>
-          <Grid Paper xs={8}>
-            <Paper></Paper>
-          </Grid>
-        </Grid>
+
+      <Box sx={{ m: "50px", display: "flex", justifyContent: "center" }}>
+        <UserCard />
       </Box>
     </>
   );

8fe2d3578e9ed4546f9327a5504e7d1764120f5c
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-08-09 22:43:12 -0700

    Now, when you click on reload button you see the incoming invitations table skeleton for at least 1 second to give the sensation that someting ocured in the back

--- upsolver-ui/src/components/InboxScreen/InboxScreen.jsx
+++ upsolver-ui/src/components/InboxScreen/InboxScreen.jsx
@@ -18,8 +18,10 @@ import RefreshIcon from "@mui/icons-material/Refresh";
 import Subtitle from "../Subtitle/Subtitle";
 import { RANKING_COLORS } from "../../constants/config.js";
 import { BACK_END_BASE_URL } from "../../constants/urls.js";
+import IncomingInvitationsSkeleton from "../IncomingInvitationsSkeleton/IncomingInvitationsSkeleton";
 
 export default function InvoxScreen() {
+  const [loading, setLoading] = React.useState(false);
   const { user } = useContext(UserContext);
 
   const [invitations, setInvitations] = React.useState([]);
@@ -86,91 +88,103 @@ export default function InvoxScreen() {
     <>
       <Navbar />
       <Subtitle>Inbox</Subtitle>
-      <Box sx={{ m: "20px" }}>
-        <TableContainer component={Paper}>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-              m: "20px",
-            }}
-          >
-            <Typography variant="h6">pending Invitations</Typography>
-            <IconButton onClick={fetchInvitations} size="large">
-              <RefreshIcon />
-            </IconButton>
-          </Box>
-          <Divider />
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Inviter</TableCell>
-                <TableCell>Team</TableCell>
-                <TableCell>Role</TableCell>
-                <TableCell>Invited On</TableCell>
-                <TableCell>Actions</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {invitations.map((invitation) => (
-                <TableRow key={invitation.id}>
-                  <TableCell>
-                    <Stack direction="row" spacing="16px" alignItems="center">
-                      <Avatar
-                        src={invitation.inviterAvatar}
-                        variant="square"
-                        sx={{ width: 50, height: 50 }}
-                      />
-                      <Box sx={{ display: "flex", flexDirection: "column" }}>
-                        <Typography variant="body1" noWrap>
-                          {invitation.inviterName}
-                        </Typography>
-                        <Typography
-                          noWrap
-                          variant="subtitle2"
-                          color={RANKING_COLORS[invitation.inviterRank]}
-                        >
-                          {invitation.inviterUsername}
-                        </Typography>
-                      </Box>
-                    </Stack>
-                  </TableCell>
-                  <TableCell>
-                    <Typography variant="body1">{invitation.team}</Typography>
-                  </TableCell>
-                  <TableCell>
-                    <Typography variant="body1">{invitation.role}</Typography>
-                  </TableCell>
-                  <TableCell>
-                    <Typography variant="body1">
-                      {new Date(invitation.createdAt).toLocaleDateString()}
-                    </Typography>
-                  </TableCell>
-                  <TableCell>
-                    <Stack direction="row" spacing={2}>
-                      <Button
-                        variant="contained"
-                        color="success"
-                        onClick={() => acceptInvitation(invitation.id)}
-                      >
-                        Accept
-                      </Button>
-                      <Button
-                        variant="contained"
-                        color="error"
-                        onClick={() => rejectInvitation(invitation.id)}
-                      >
-                        Reject
-                      </Button>
-                    </Stack>
-                  </TableCell>
+      {loading ? (
+        <IncomingInvitationsSkeleton />
+      ) : (
+        <Box sx={{ m: "20px" }}>
+          <TableContainer component={Paper}>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+                m: "20px",
+              }}
+            >
+              <Typography variant="h6">pending Invitations</Typography>
+              <IconButton
+                onClick={async () => {
+                  setLoading(true);
+                  await fetchInvitations();
+                  await new Promise((resolve) => setTimeout(resolve, 1000));
+                  setLoading(false);
+                }}
+                size="large"
+              >
+                <RefreshIcon />
+              </IconButton>
+            </Box>
+            <Divider />
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Inviter</TableCell>
+                  <TableCell>Team</TableCell>
+                  <TableCell>Role</TableCell>
+                  <TableCell>Invited On</TableCell>
+                  <TableCell>Actions</TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Box>
+              </TableHead>
+              <TableBody>
+                {invitations.map((invitation) => (
+                  <TableRow key={invitation.id}>
+                    <TableCell>
+                      <Stack direction="row" spacing="16px" alignItems="center">
+                        <Avatar
+                          src={invitation.inviterAvatar}
+                          variant="square"
+                          sx={{ width: 50, height: 50 }}
+                        />
+                        <Box sx={{ display: "flex", flexDirection: "column" }}>
+                          <Typography variant="body1" noWrap>
+                            {invitation.inviterName}
+                          </Typography>
+                          <Typography
+                            noWrap
+                            variant="subtitle2"
+                            color={RANKING_COLORS[invitation.inviterRank]}
+                          >
+                            {invitation.inviterUsername}
+                          </Typography>
+                        </Box>
+                      </Stack>
+                    </TableCell>
+                    <TableCell>
+                      <Typography variant="body1">{invitation.team}</Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Typography variant="body1">{invitation.role}</Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Typography variant="body1">
+                        {new Date(invitation.createdAt).toLocaleDateString()}
+                      </Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Stack direction="row" spacing={2}>
+                        <Button
+                          variant="contained"
+                          color="success"
+                          onClick={() => acceptInvitation(invitation.id)}
+                        >
+                          Accept
+                        </Button>
+                        <Button
+                          variant="contained"
+                          color="error"
+                          onClick={() => rejectInvitation(invitation.id)}
+                        >
+                          Reject
+                        </Button>
+                      </Stack>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
+      )}
     </>
   );
 }
